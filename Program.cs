2b15534599cd9de27740e34dcdc0ae8904a82b49
исп-22 <-22@LAB30-04>
2024-12-09 13:27:15 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿/*Дана непустая последовательность целых чисел, оканчивающаяся -5. Найти: 
+а)количество четных чисел последовательности; 
+б)количество положительных чисел последовательности.*/
+
+Console.WriteLine("Введите числа (последовательность заканчивается -5):");
+int countEven = 0;
+int countPositive = 0; 
+int number;
+do
+{
+    number = int.Parse(Console.ReadLine());
+    if (number == -5)
+        break; 
+    if (number % 2 == 0)
+    {
+        countEven++;
+    }
+    if (number > 0) 
+    {
+        countPositive++;
+    }
+} while (true);
+Console.WriteLine($"Четных чисел: {countEven}");
+Console.WriteLine($"Положительных чисел: {countPositive}");
